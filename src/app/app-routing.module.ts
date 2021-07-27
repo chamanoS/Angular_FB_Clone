253b51a67ad850261981b0fb1b63b6d74fd03ae5
Chamano Ramashia <chamano.sr@gmail.com>
2021-07-27 10:10:59 +0200

    project structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './components/home/home.module';
+import { LoginComponent } from './components/login/login.component';
+import { FacebookGuard } from './guard/facebook.guard';
+
+const routes: Routes = [
+  {
+    path:"", loadChildren: ()=> import('./components/home/home.module').then(m=> m.HomeModule),
+    canActivate: [FacebookGuard]
+  },
+  {
+    path:"login", component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
